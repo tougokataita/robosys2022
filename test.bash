6e9ee39b8e0cf657bd367bc384fda5abbaf672f6
Tougo Kataita <105togo105togo@gmail.com>
2023-11-28 20:20:38 +0900

    Add test

--- test.bash
+++ test.bash
@@ -1,14 +1,23 @@
 #!/bin/bash 
 
 ng () {
-	echo NG at Line $1
+	echo ${1}行目が間違えてる
 	res=1
 }
 
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+output="0 + 1 = 1
+1 + 2 = 3
+3 + 3 = 6
+6 + 4 = 10
+10 + 5 = 15
+
+1 ~ 5の合計 : 15
+平均値 : 3.0
+奇数"
+[ "${out}" = "${output}" ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]       || ng ${LINENO}
