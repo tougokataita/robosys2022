7fbc7fe22cbdb3ffb294c21127ce58327cd12847
Tougo Kataita <105togo105togo@gmail.com>
2023-11-28 20:28:57 +0900

    課題1

--- test.bash
+++ test.bash
@@ -19,6 +19,17 @@ output="0 + 1 = 1
 奇数"
 [ "${out}" = "${output}" ] || ng ${LINENO}
 
+
+out=$(seq 2 | ./plus)
+output="0 + 1 = 1
+1 + 2 = 3
+
+1 ~ 2の合計 : 3
+平均値 : 1.5
+奇数
+素数"
+[ "${out}" = "${output}" ] || ng ${LINENO}
+
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]       || ng ${LINENO}
 [ "${out}" = "" ]  || ng ${LINENO}
